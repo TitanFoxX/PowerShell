8e9eec023635ffa8db7867fbd9d71823c0d2175e
Ruud Bos <42388979+TitanFoxX@users.noreply.github.com>
2019-06-04 09:30:47 +0200

    Add files via upload
    This script will convert a plain string into a encrypted password. The second part of the script will allow it to be converted back to plain text.

---
+++ Credentials_EncryptDecrypt_Pass.ps1
@@ -0,0 +1,12 @@
+﻿# Type or copy-paste your plain text password in the pop-up
+# The password is then encrypted and stored in the $Encrypted_Pass variable
+
+$Encrypted_Pass = Read-Host -Prompt "Enter your password for encryption" -AsSecureString | ConvertFrom-SecureString
+Write-Host "The encrypted password is : $Encrypted_Pass"
+
+# If the application in which you want to use the password doesn't support
+# the use of encrypted passwords, this script can be used to decrypt it first
+
+$Unencrypted_Pass = $Encrypted_Pass | ConvertTo-SecureString
+$Unencrypted_Pass = [System.Runtime.InteropServices.marshal]::PtrToStringAuto([System.Runtime.InteropServices.marshal]::SecureStringToBSTR($Unencrypted_Pass))
+Write-Host "The unencrypted password is : $Unencrypted_Pass"
