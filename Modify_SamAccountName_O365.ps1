4270df289c301bd19f367e80c0e2dca9ea3bb2cf
Ruud Bos <42388979+TitanFoxX@users.noreply.github.com>
2019-06-04 09:23:01 +0200

    Add files via upload
    This script will set the ExtensionAttribute7 parameter to "Office365" within the specified AD-account.
    Once synchronized with Office 365 Online, licenses can then be assigned to this account.

---
+++ Modify_SamAccountName_O365.ps1
@@ -0,0 +1,13 @@
+﻿while($true){
+    $Username = Read-Host "Please enter a username "
+    $SAMuser = Get-ADUser -Filter {SamAccountName -like $Username}
+
+    If($SAMuser){
+        Write-Host "SAM Name Found : $SAMuser"
+        Set-ADUser $SAMuser -add @{"extensionattribute7"="Office365"}
+        Get-ADUser $SAMuser -Properties extensionAttribute7
+    }
+    Else{
+        Write-Host "Error : Name not found !"
+    }
+}
