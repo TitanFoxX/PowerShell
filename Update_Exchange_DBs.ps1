1b3f27bb0cb879e6115aa7a0c12b44472769d039
Ruud Bos <42388979+TitanFoxX@users.noreply.github.com>
2019-06-04 09:18:50 +0200

    Add files via upload
    This script will query available Exchange DB's and scan Active Directory for disconnected mailboxes.
    The DB's will then be synced.

---
+++ Update_Exchange_DBs.ps1
@@ -0,0 +1,5 @@
+﻿# Load required SnapIn for Powershell.
+Add-PSSnapin Microsoft.Exchange.Management.PowerShell.E2010
+
+# Get all Exchange DB's first, then send (pipe) them to the Clean-cmdlet.
+Get-MailboxDatabase | Clean-MailboxDatabase
