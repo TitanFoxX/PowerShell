95b49b672034ce1d2dfd6e33c9a25a41af23b0ab
Ruud Bos <42388979+TitanFoxX@users.noreply.github.com>
2019-06-25 15:33:41 +0200

    Add files via upload
    Simple script to export encrypted credentials to a XML-file.

---
+++ Encryption_Creds2XML.ps1
@@ -0,0 +1,7 @@
+﻿# - Display pop-up for filling in your AD-credentials.
+
+$Credentials = Get-Credential
+
+# - Export encrypted credentials to XML-file.
+
+$Credentials | Export-CliXml -Path '<file path>\credentials.xml'
