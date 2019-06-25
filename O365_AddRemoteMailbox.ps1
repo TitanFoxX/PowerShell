b12ca6aab92c4c957c93e4b3640e6d2978fb9a01
Ruud Bos <42388979+TitanFoxX@users.noreply.github.com>
2019-06-25 15:13:47 +0200

    Add files via upload
    This script will enable and set a remote mailbox for selected user. This is required when mailboxes have been migrated from on-premise Exchange server towards Office 356.

---
+++ O365_AddRemoteMailbox.ps1
@@ -0,0 +1,8 @@
+﻿# - Load required Snap-in for Microsoft Exchange.
+
+Add-PSSnapin Microsoft.Exchange.Management.PowerShell.E2010
+
+# - Enable and set RemoteMailbox for selected user.
+
+Enable-RemoteMailbox "<full AD name>" -PrimarySmtpAddress "<e-mail address>" -Alias "<SamAccountName>" -RemoteRoutingAddress "<e-mail address>@<company>.mail.onmicrosoft.com"
+Set-RemoteMailbox "<SamAccountName>" -EmailAddressPolicyEnabled $False -EmailAddresses @{add='<e-mail address>@<company>.mail.onmicrosoft.com'} 
