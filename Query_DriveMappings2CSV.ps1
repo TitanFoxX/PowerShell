40a5716073c0d6910e5050c7ea46dd1a5af39093
Ruud Bos <42388979+TitanFoxX@users.noreply.github.com>
2019-06-25 14:49:40 +0200

    Add files via upload
    This simple script will query all mapped logical drives and store the information to a CSV file.

---
+++ Query_DriveMappings2CSV.ps1
@@ -0,0 +1,33 @@
+﻿# - Keeping everything nice and quiet.
+
+$ErrorActionPreference = 'SilentlyContinue'
+
+# - Convert current date to NL format
+
+$DateConv = Get-Date -Format 'dd-MM-yyyy'
+
+# - Store computername from which this script is run into a variable.
+
+$ComputerName = $env:COMPUTERNAME
+
+# - Define file path to store CSV-results file.
+
+$FilePath = '<fill in desired path>'
+
+# - Test if defined file path exists.
+
+$Result = Test-Path $FilePath'\'$ComputerName
+
+# - If the file path does not exist yet, create it.
+
+If($Result -eq $False){
+    New-Item -ItemType Directory -Path $FilePath -Name $ComputerName -Force | Out-Null
+}
+
+# - Store all mapped logical disks into #Output.
+
+$Output = Get-WmiObject Win32_MappedLogicalDisk | Select-Object PSComputerName,Name,Providername | FT -HideTableHeaders
+
+# - Write content of $Output to CSV file in defined file path.
+
+$Output | Out-File $FilePath'\'$ComputerName'\'$ComputerName'__'$DateConv.csv
