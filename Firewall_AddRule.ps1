620d17f550530ce53269643bb3f50ce658168bfb
Ruud Bos <42388979+TitanFoxX@users.noreply.github.com>
2019-06-26 10:28:39 +0200

    Add files via upload
    Script for adding Windows Firewall rules.

---
+++ Firewall_AddRule.ps1
@@ -0,0 +1,25 @@
+﻿# - Create a firewall rule on this system.
+
+# - Choose a name for the new rule.
+
+$DisplayName = ""
+
+# - Define direction of rule. Can be "Inbound" or "Outbound".
+
+$Direction = ""
+
+# - Define ports for this rule. More than one port can be entered, seperated by comma's.
+
+$PortNumbers = ""
+
+# - Define the protocol. Can be TCP or UDP.
+
+$Protocol = ""
+
+# - Action can be either "Allow" or "Block".
+
+$Action = ""
+
+# - Command for executing this action.
+
+New-NetFirewallRule -DisplayName "$DisplayName" -Direction $Direction -LocalPort $PortNumbers -Protocol $Protocol -Action $Action
