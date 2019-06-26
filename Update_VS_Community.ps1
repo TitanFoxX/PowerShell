60abfa3df4468a765b03e091d7cfe88cd21c22f0
Ruud Bos <42388979+TitanFoxX@users.noreply.github.com>
2019-06-26 09:02:21 +0200

    Add files via upload
    Simple script to update an installed Visual Studio Community installation. Can be practical for scheduled updating.

---
+++ Update_VS_Community.ps1
@@ -0,0 +1,9 @@
+﻿# - Check if Visual Studio Community is already installed at the target location.
+# - If located, execute the vs_installer.exe to update the current installation.
+
+If(Test-Path -Path "C:\Program Files (x86)\Microsoft Visual Studio\Installer"){
+    Set-Location -Path "C:\Program Files (x86)\Microsoft Visual Studio\Installer"
+    .\vs_installer.exe update --quiet --norestart --installpath "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community"
+}
+Else{
+}
