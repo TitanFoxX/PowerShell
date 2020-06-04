1a52d46e5d0f6262659cad283a8a616f1eee1664
Ruud Bos <42388979+TitanFoxX@users.noreply.github.com>
2020-06-04 09:41:42 +0200

    Add files via upload

---
+++ Query - SQL - SplunkTempTable.ps1
@@ -0,0 +1,20 @@
+﻿# - Load required Snapins.
+
+Add-PSSnapin SqlServerCmdletSnapin100
+Add-PSSnapin SqlServerProviderSnapin100
+
+# - Define query for getting 
+
+Function Test-SQLTableExists{ 
+    Param ($Instance,$Database,$TableName)
+    $Return = $SQL = $DataTable = $Null
+    $SQL = "SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'$TableName'"
+    $DataTable = Invoke-Sqlcmd -ServerInstance $Instance -Database $Database -Query $SQL
+    If($DataTable){$Return = $True}
+    Else{$Return = $False}
+    $Return
+}
+
+# - Execute Query.
+
+Test-SQLTableExists -Instance "" -Database "" -TableName TempTableSplunk
