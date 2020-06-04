116fc09db70577eb008b75843abedfc629a81ba6
Ruud Bos <42388979+TitanFoxX@users.noreply.github.com>
2020-06-04 09:17:23 +0200

    Add files via upload

---
+++ Query - Task - Specific.ps1
@@ -0,0 +1,9 @@
+﻿$Servers = @()
+$Servers += Get-ADComputer -Filter {enabled -eq $true} -Properties Name | Where-Object {($_.Name -like "*<enter remote computer>*")} | Select-Object Name -ExpandProperty Name
+
+Foreach($Server in $Servers){
+    $Result = Get-ScheduledTask -Taskname '*<enter scheduled taskname>*' -ErrorAction SilentlyContinue
+    If($Result){
+        "Task found on server $Server"
+    }
+}
